f6a0e011a9ced26d1411f2a05ff45d758877a0ff
mandytuesday <06amandarosario@gmail.com>
2025-03-29 22:57:35 +0800

    Add files via upload

---
+++ C++DICTIONARY.cxx
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string a;
+    
+    cout<<"Please enter your word:";
+    cin>>a;
+    
+    if(a=="Auto recloser"){
+        cout<<"A circuit protection device for overhead power distribution lines which briefly interrupts a circuit when a fault is detected";
+    }else if(a=="Broadcasting"){
+        cout<<"Transmission of a signal to many receivers";
+    }else if(a=="Bushing"){
+        cout<<"An electrical fitting used to connect external conductors to the interior of apparatus";
+    }else if(a=="Cathode"){
+        cout<<"An electrode that allows electron to flow in a device, such as a battery or electron tube";
+    }else if(a=="Choke"){
+        cout<<"An induction coil used to block alternating current and pass direct current";
+    }else if(a=="Demodulation"){
+        cout<<"The process of recovery of information from a modulated carrier";
+    }else if(a=="Electrification"){
+        cout<<"Applying electric power to a process that was previously done by other means";
+    }else if(a=="Electrodynamics"){
+        cout<<"The branch of physics that studies electrical charges and electrical current";
+    }else if(a=="Ferroelectricity"){
+        cout<< "The property of materials that spotaneously maintain an electrical polarization";
+    }else if(a=="Firmware"){
+        cout<<"Software of a computer that is never or rarely altered during its working life";
+    }else if(a=="Gyrotron"){
+        cout<< "A high powered vacuum tube oscillator that can produce microwave frequencies up to hundreds pf GigaHertz at power levels up to Mega Watts";
+    }else if(a=="Heterodyne"){
+        cout<<"The process of mixing signals of a number of frequencies to produce new frequencies";
+    }else if(a=="Inverter"){
+        cout<<"A system that converts direct current power to alternating current,without the use of rotating machines";
+    }else if(a=="Jitter"){
+        cout<<"Deviation from the true periodicity of a periodic signal";
+    }else if(a=="Klystron"){
+        cout<<"A type of microwave oscillator vacuum tube that is used in radar and other communication systems";
+    }else if(a=="LAN"){
+        cout<< "Local Area Network, an interconnection of computers over a building or small campus";
+    }else if(a=="Mainframe computer"){
+        cout<<"A large centralized computer system, used for large volumes of data or supporting multiple interactive terminals";
+    }else if(a=="Negawatt computer"){
+        cout<<"Theoretical unit of power representing an amount of electrical power saved";
+    }else if(a=="Oscilloscope"){
+        cout<<"An instrument for graphically displaying a waveform as a function of time";
+    }else if(a=="Permeability"){
+        cout<<"The amount of magnetisation in a material resulting from an applied magnetic field";
+    }else if(a=="Quantization"){
+        cout<<"Analog to digital conversion, changing a continuously varying analog signal to discrete digital numbers";
+    }else if(a=="Radar"){
+        cout<<"observes the speed and position of objects by reflected radio waves";
+    }else if(a=="Segmentation"){
+        cout<<"A step in digital image processing that groups picture element of an image that notionally represents some physically significant property of the imaged object";
+    }else if(a=="Telecommunication"){
+        cout<< "The field that deals with transmission of information over distances longer than can be covered by an unaided human";
+    }else if(a=="Ultrasonics"){
+        cout<<"Term for the field of study pertaining to pressure oscillations in air or other media that are above the range of human hearing";
+    }else if(a=="Valve"){
+        cout<<"A switching element in high voltage direct current converter";
+    }else if(a=="Waveguide"){
+        cout<<"A tubular structure that guides electromagnetic waves, much used as microwave frequencies";
+    }else if(a=="X-ray"){
+        cout<<"Electromagnetic radiation with wavelengths shorter than ten nanometers";
+    }else if(a=="Yagi Antenna"){
+        cout<< "A type of radio antenna using a feeder element, one or more parasitic reflector element, and one or more director parasitic element to provide a directional characteristic";
+    }else if(a=="Z-transform"){
+        cout<<"A mathematical operation that converts a set of evenly spaced measurements of an analog signal into a series of frequency components";
+    }else{
+        cout<<"Cannot find word.";
+    }
+    return 0;
+}
+  
+  
+  
+    
